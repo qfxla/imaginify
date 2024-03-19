c4ef45fcb64726e431d137325e5b507ae2658d8c
qfxla <2690357721@qq.com>
2024-03-19 13:59:01 +0800

    init

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -22,4 +22,8 @@ export const connectToDatabase = async () => {
 
     cached.promise = cached.promise ||  mongoose.connect
     (MONGODB_URL, { dbName: 'imaginify', bufferCommands: false});
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
 }
